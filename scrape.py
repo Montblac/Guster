260a5481cc0c1ee6528a6b1181df2e51e87652c4
Montblac <montblac95@gmail.com>
2018-12-03 14:12:39 -0800

    Minor adjustments to comments, formatting Renamed method open --> initialize

--- scrape.py
+++ scrape.py
@@ -4,18 +4,18 @@ from bs4 import BeautifulSoup
 
 psych_url = 'https://psychusa.fandom.com/wiki/List_of_Gus%27_Nicknames'
 
+
 class URLParser:
     def __init__(self, url=psych_url):
         self.url = url
         self.page = None
 
-    def open(self):
+    def initialize(self):
         """
-        Opens the url
-        :return: URL Object
+        Opens the url and stores the URL object
+        :return: None
         """
         self.page = urllib.request.urlopen(self.url)
-        return self.page
 
     def read(self):
         """
@@ -27,14 +27,14 @@ class URLParser:
     def names(self):
         """
         Retrieves the nicknames from the page
-        :return: List of Names
+        :return: list
         """
         soup = BeautifulSoup(self.page, 'html.parser')
         div = soup.find("div", {"id": "mw-content-text"}).parent
         info = div.find_all('ul')
         seasons = [season.text.split("\n")[:-1] for season in info[:-2]]
-        names = []
 
+        names = []
         p1 = re.compile('[/;,]+\s*')
         p2 = re.compile('^[^(]*')
 
