b984bc0244c0f2b43f7a0bdc00de06576e8b800a
Montblac <montblac95@gmail.com>
2019-03-18 15:50:29 -0700

    Removed unnecessary imports and cleaned up code a bit

--- gui.py
+++ gui.py
@@ -1,13 +1,13 @@
-import os
-import tkinter as tk
+from tkinter import Tk, Button
 from PIL import ImageTk, Image
 
+
 class Window:
-    def __init__(self, phrase=None):
-        self.phrase = phrase
+    def __init__(self, name=None):
+        self.name = name
         self.img = None
 
-        self.root = tk.Tk()
+        self.root = Tk()
         self.root.title('Bruton Gaster')
 
         # Fixed size 600x600
@@ -18,10 +18,21 @@ class Window:
         y_offset = int(self.root.winfo_screenheight() / 2 - 600 / 2)
         self.root.geometry("+{}+{}".format(x_offset, y_offset))
 
+        self.button = Button(self.root, text="Press me", command=self.show)
+        self.button.pack()
+
     def show(self):
+        """
+        Shows a random image of Gus and nickname
+        :return: None
+        """
         pass
 
     def update(self):
+        """
+        Updates current image and nickname
+        :return: None
+        """
         pass
 
     def run(self):
