922a4649bd98ac79d94b1a4e94f280e70398fa31
Montblac <montblac95@gmail.com>
2019-03-19 14:23:23 -0700

    Added ImageGenerator to main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import name
+import image
 import gui
 
 if __name__ == '__main__':
     namegen = name.NameGenerator()
     names = namegen.names
 
-    win = gui.Window(names=names, images=None)
+    imgen = image.ImageGenerator()
+    images = imgen.images
+
+    win = gui.Window(names=names, images=images)
     win.run()
