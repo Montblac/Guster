49392f285e9b945495309d17d6bcd62343b947b4
Montblac <montblac95@gmail.com>
2019-03-19 12:31:04 -0700

    Removed unused greetgen import

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 import name
-import greetgen
 
 if __name__ == '__main__':
-    ng = name.NameGenerator()
-    ng.initialize()
-
-    gg = greetgen.GreetGenerator()
-    phrase = gg.get_greeting()
-
-    print(phrase.format(ng.get_name()))
+    namegen = name.NameGenerator()
